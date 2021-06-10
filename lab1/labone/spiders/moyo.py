7e66408540ef7481b991b6cb380706b084dfc6c9
Renata <rdzhergalova@gmail.com>
2021-06-10 17:52:55 +0300

    Updated moyo url

--- lab1/labone/spiders/moyo.py
+++ lab1/labone/spiders/moyo.py
@@ -6,7 +6,7 @@ import scrapy
 class MoyoSpider(scrapy.Spider):
     name = 'moyo'
     allowed_domains = ['www.moyo.ua']
-    start_urls = ['https://www.moyo.ua/detskij_mir/igrushki/konstruktory/?filters=3967_355236,355245&brands=lego']
+    start_urls = ['https://www.moyo.ua/detskij_mir/igrushki/konstruktory/?filters=3967_355245&brands=lego']
 
     def parse(self, response: Response):
         pages_num = 20
